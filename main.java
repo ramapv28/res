fd1586fceaf535913d33dd079e5a9877aec5833f
Testing Trainer <revive.techtrainer@gmail.com>
2026-02-04 17:22:33 +0530

    first commit

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 class main{
     public static void main (String[] args){
-        System.out.println("Welcome to the automation class");
+        System.out.println("Welcome to the automation java selenium class");
     }
 }
